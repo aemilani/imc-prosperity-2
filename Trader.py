495044a360edb9c220f5d36cce1f125264f208cd
aemilani <aealieftekhary@gmail.com>
2024-04-07 13:50:32 +0200

    Entered acceptable prices

--- Trader.py
+++ Trader.py
@@ -13,7 +13,19 @@ class Trader:
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            acceptable_price = 10000  # Participant should calculate this value
+
+            price_amethysts = 10000
+            price_starfruit = 5000
+
+            if (len(order_depth.sell_orders) != 0) and (len(order_depth.buy_orders) != 0):
+                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                price_starfruit = (best_ask + best_bid) / 2
+
+            if product == 'AMETHYSTS':
+                acceptable_price = price_amethysts  # Participant should calculate this value
+            elif product == 'STARFRUIT':
+                acceptable_price = price_starfruit
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(
                 len(order_depth.sell_orders)))
