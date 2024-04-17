399024346c52d814db80deae067b63cede3f8ca9
aemilani <aealieftekhary@gmail.com>
2024-04-17 22:08:04 +0200

    2024-04-17 22:08

--- round_3.py
+++ round_3.py
@@ -10,172 +10,170 @@ class Trader:
         conversions = 0
         traderData = 'SAMPLE'
         # Retrieve previous state -------------------------------------------------------------------------------------
-        # if state.traderData:
-        #     try:
-        #         previous_state = jsonpickle.decode(state.traderData)
-        #         bids = previous_state.get('bids', {})
-        #         asks = previous_state.get('asks', {})
-        #     except Exception:
-        #         print('JSON Decode error encountered.')
-        #         bids = {}
-        #         asks = {}
-        # else:
-        #     bids = {}
-        #     asks = {}
+        if state.traderData:
+            try:
+                previous_state = jsonpickle.decode(state.traderData)
+                bids = previous_state.get('bids', {})
+                asks = previous_state.get('asks', {})
+            except Exception:
+                print('JSON Decode error encountered.')
+                bids = {}
+                asks = {}
+        else:
+            bids = {}
+            asks = {}
 
         # Trading -----------------------------------------------------------------------------------------------------
         result = {}
 
         # AMETHYSTS ---------------------------------------------------------------------------------------------------
-        # if 'AMETHYSTS' in state.order_depths:
-        #     product = 'AMETHYSTS'
-        #     print(product)
-        #
-        #     order_depth: OrderDepth = state.order_depths[product]
-        #
-        #     if product in state.position:
-        #         if state.position[product]:
-        #             curr_position: int = state.position[product]
-        #         else:
-        #             curr_position = 0
-        #     else:
-        #         curr_position = 0
-        #
-        #     print(f'Current position: {curr_position}')
-        #
-        #     fair_value = 10000
-        #     max_position = 20
-        #
-        #     max_buy_size = max_position - curr_position
-        #     max_sell_size = -max_position - curr_position
-        #
-        #     # Market making
-        #     thr_l = fair_value - 2
-        #     thr_h = fair_value + 2
-        #     buy_price = thr_l
-        #     sell_price = thr_h
-        #
-        #     orders: List[Order] = []
-        #     if max_buy_size > 0:
-        #         print("BUY", str(max_buy_size) + "x", buy_price)
-        #         orders.append(Order(product, buy_price, max_buy_size))
-        #     if max_sell_size < 0:
-        #         print("SELL", str(max_sell_size) + "x", sell_price)
-        #         orders.append(Order(product, sell_price, max_sell_size))
-        #
-        #     result[product] = orders
+        if 'AMETHYSTS' in state.order_depths:
+            product = 'AMETHYSTS'
+            print(product)
+
+            if product in state.position:
+                if state.position[product]:
+                    curr_position: int = state.position[product]
+                else:
+                    curr_position = 0
+            else:
+                curr_position = 0
+
+            print(f'Current position: {curr_position}')
+
+            fair_value = 10000
+            max_position = 20
+
+            max_buy_size = max_position - curr_position
+            max_sell_size = -max_position - curr_position
+
+            # Market making
+            thr_l = fair_value - 2
+            thr_h = fair_value + 2
+            buy_price = thr_l
+            sell_price = thr_h
+
+            orders: List[Order] = []
+            if max_buy_size > 0:
+                print("BUY", str(max_buy_size) + "x", buy_price)
+                orders.append(Order(product, buy_price, max_buy_size))
+            if max_sell_size < 0:
+                print("SELL", str(max_sell_size) + "x", sell_price)
+                orders.append(Order(product, sell_price, max_sell_size))
+
+            result[product] = orders
 
         # STARFRUIT ---------------------------------------------------------------------------------------------------
-        # if 'STARFRUIT' in state.order_depths:
-        #     product = 'STARFRUIT'
-        #     print(product)
-        #     if product not in bids:
-        #         bids[product] = []
-        #     if product not in asks:
-        #         asks[product] = []
-        #
-        #     order_depth: OrderDepth = state.order_depths[product]
-        #
-        #     if product in state.position:
-        #         if state.position[product]:
-        #             curr_position: int = state.position[product]
-        #         else:
-        #             curr_position = 0
-        #     else:
-        #         curr_position = 0
-        #
-        #     print(f'Current position: {curr_position}')
-        #
-        #     if len(order_depth.buy_orders) != 0:
-        #         best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-        #     else:
-        #         best_bid, best_bid_amount = None, None
-        #     if len(order_depth.sell_orders) != 0:
-        #         best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-        #     else:
-        #         best_ask, best_ask_amount = None, None
-        #
-        #     bids[product].append(best_bid)
-        #     asks[product].append(best_ask)
-        #
-        #     if len(bids[product]) > 100:
-        #         bids[product] = bids[product][1:]
-        #         asks[product] = asks[product][1:]
-        #
-        #     traderData = jsonpickle.encode({"bids": bids, "asks": asks})
-        #
-        #     max_position = 20
-        #     max_buy_size = max_position - curr_position
-        #     max_sell_size = -max_position - curr_position
-        #
-        #     # Market making
-        #     bid_arr = np.array(bids[product])
-        #     ask_arr = np.array(asks[product])
-        #
-        #     orders: List[Order] = []
-        #     w_roll = 9
-        #     if len(bid_arr) >= w_roll:
-        #         mid_arr = (bid_arr + ask_arr) / 2
-        #         mid_ma = mid_arr[-w_roll:].mean()
-        #         fair_value = mid_ma
-        #         if max_buy_size > 0:
-        #             buy_price = int(np.floor(fair_value)) - 1
-        #             print("BUY", str(max_buy_size) + "x", buy_price)
-        #             orders.append(Order(product, buy_price, max_buy_size))
-        #         if max_sell_size < 0:
-        #             sell_price = int(np.ceil(fair_value)) + 1
-        #             print("SELL", str(max_sell_size) + "x", sell_price)
-        #             orders.append(Order(product, sell_price, max_sell_size))
-        #
-        #     result[product] = orders
+        if 'STARFRUIT' in state.order_depths:
+            product = 'STARFRUIT'
+            print(product)
+            if product not in bids:
+                bids[product] = []
+            if product not in asks:
+                asks[product] = []
+
+            order_depth: OrderDepth = state.order_depths[product]
+
+            if product in state.position:
+                if state.position[product]:
+                    curr_position: int = state.position[product]
+                else:
+                    curr_position = 0
+            else:
+                curr_position = 0
+
+            print(f'Current position: {curr_position}')
+
+            if len(order_depth.buy_orders) != 0:
+                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+            else:
+                best_bid, best_bid_amount = None, None
+            if len(order_depth.sell_orders) != 0:
+                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+            else:
+                best_ask, best_ask_amount = None, None
+
+            bids[product].append(best_bid)
+            asks[product].append(best_ask)
+
+            if len(bids[product]) > 100:
+                bids[product] = bids[product][1:]
+                asks[product] = asks[product][1:]
+
+            traderData = jsonpickle.encode({"bids": bids, "asks": asks})
+
+            max_position = 20
+            max_buy_size = min(max_position, max_position - curr_position)
+            max_sell_size = max(-max_position, -max_position - curr_position)
+
+            # Market making
+            bid_arr = np.array(bids[product])
+            ask_arr = np.array(asks[product])
+
+            orders: List[Order] = []
+            w_roll = 9
+            if len(bid_arr) >= w_roll:
+                mid_arr = (bid_arr + ask_arr) / 2
+                mid_ma = mid_arr[-w_roll:].mean()
+                fair_value = mid_ma
+                if max_buy_size > 0:
+                    buy_price = int(np.floor(fair_value)) - 1
+                    print("BUY", str(max_buy_size) + "x", buy_price)
+                    orders.append(Order(product, buy_price, max_buy_size))
+                if max_sell_size < 0:
+                    sell_price = int(np.ceil(fair_value)) + 1
+                    print("SELL", str(max_sell_size) + "x", sell_price)
+                    orders.append(Order(product, sell_price, max_sell_size))
+
+            result[product] = orders
 
         # ORCHIDS -----------------------------------------------------------------------------------------------------
-        # if 'ORCHIDS' in state.order_depths:
-        #     product = 'ORCHIDS'
-        #     print(product)
-        #
-        #     order_depth: OrderDepth = state.order_depths[product]
-        #
-        #     if product in state.position:
-        #         if state.position[product]:
-        #             curr_position: int = state.position[product]
-        #         else:
-        #             curr_position = 0
-        #     else:
-        #         curr_position = 0
-        #
-        #     print(f'Current position: {curr_position}')
-        #
-        #     conversions = -curr_position
-        #
-        #     max_position = 100
-        #     max_buy_size = max_position - curr_position
-        #     max_sell_size = -max_position - curr_position
-        #
-        #     bid_price = state.observations.conversionObservations['ORCHIDS'].bidPrice
-        #     ask_price = state.observations.conversionObservations['ORCHIDS'].askPrice
-        #     transport_fees = state.observations.conversionObservations['ORCHIDS'].transportFees
-        #     export_tariff = state.observations.conversionObservations['ORCHIDS'].exportTariff
-        #     import_tariff = state.observations.conversionObservations['ORCHIDS'].importTariff
-        #
-        #     # Arbitrage
-        #     effective_south_bid_price = bid_price - transport_fees - export_tariff
-        #     effective_south_ask_price = ask_price + transport_fees + import_tariff
-        #
-        #     north_bid_1, north_bid_1_amount = list(order_depth.buy_orders.items())[0]
-        #     north_ask_1, north_ask_1_amount = list(order_depth.sell_orders.items())[0]
-        #
-        #     orders: List[Order] = []
-        #     if north_bid_1 > effective_south_ask_price:  # short
-        #         sell_size = max(max_sell_size, -north_bid_1_amount)
-        #         print("SELL", str(sell_size) + "x", north_bid_1)
-        #         orders.append(Order(product, north_bid_1, sell_size))
-        #     elif north_ask_1 < effective_south_bid_price:  # long
-        #         buy_size = min(max_buy_size, -north_ask_1_amount)
-        #         print("BUY", str(buy_size) + "x", north_ask_1)
-        #         orders.append(Order(product, north_ask_1, buy_size))
-        #
-        #     result[product] = orders
+        if 'ORCHIDS' in state.order_depths:
+            product = 'ORCHIDS'
+            print(product)
+
+            order_depth: OrderDepth = state.order_depths[product]
+
+            if product in state.position:
+                if state.position[product]:
+                    curr_position: int = state.position[product]
+                else:
+                    curr_position = 0
+            else:
+                curr_position = 0
+
+            print(f'Current position: {curr_position}')
+
+            conversions = -curr_position
+
+            max_position = 100
+            max_buy_size = max_position - curr_position
+            max_sell_size = -max_position - curr_position
+
+            bid_price = state.observations.conversionObservations['ORCHIDS'].bidPrice
+            ask_price = state.observations.conversionObservations['ORCHIDS'].askPrice
+            transport_fees = state.observations.conversionObservations['ORCHIDS'].transportFees
+            export_tariff = state.observations.conversionObservations['ORCHIDS'].exportTariff
+            import_tariff = state.observations.conversionObservations['ORCHIDS'].importTariff
+
+            # Arbitrage
+            effective_south_bid_price = bid_price - transport_fees - export_tariff
+            effective_south_ask_price = ask_price + transport_fees + import_tariff
+
+            north_bid_1, north_bid_1_amount = list(order_depth.buy_orders.items())[0]
+            north_ask_1, north_ask_1_amount = list(order_depth.sell_orders.items())[0]
+
+            orders: List[Order] = []
+            if north_bid_1 > effective_south_ask_price:  # short
+                sell_size = max(max_sell_size, -north_bid_1_amount)
+                print("SELL", str(sell_size) + "x", north_bid_1)
+                orders.append(Order(product, north_bid_1, sell_size))
+            elif north_ask_1 < effective_south_bid_price:  # long
+                buy_size = min(max_buy_size, -north_ask_1_amount)
+                print("BUY", str(buy_size) + "x", north_ask_1)
+                orders.append(Order(product, north_ask_1, buy_size))
+
+            result[product] = orders
 
         # GIFT_BASKET -------------------------------------------------------------------------------------------------
         if ('CHOCOLATE' in state.order_depths) and ('STRAWBERRIES' in state.order_depths) and\
@@ -256,10 +254,13 @@ class Trader:
             diff = mid - mid_eq
             diff_mean = 379.5
             diff_std = 76.4
-            fair_diff_high = diff_mean + 0.2 * diff_std
-            fair_diff_low = diff_mean - 0.2 * diff_std
-            thr_high = diff_mean + 0.5 * diff_std
-            thr_low = diff_mean - 0.5 * diff_std
+
+            param_fair_diff = 0.29
+            param_thr = 0.57
+            fair_diff_high = diff_mean + param_fair_diff * diff_std
+            fair_diff_low = diff_mean - param_fair_diff * diff_std
+            thr_high = diff_mean + param_thr * diff_std
+            thr_low = diff_mean - param_thr * diff_std
 
             # Market taking
             if diff < thr_low:  # sell COMPOSITE, buy GIFT_BASKET
@@ -340,7 +341,7 @@ class Trader:
                         sell_size_ss -= size_ss
                     result[product].extend(orders)
 
-            elif fair_diff_high < diff < fair_diff_high:  # Liquidate positions
+            elif fair_diff_low < diff < fair_diff_high:  # Liquidate positions
                 if curr_positions['GIFT_BASKET'] < 0:  # sell COMPOSITE, buy GIFT_BASKET
                     ratio_available = min(available_ask_sizes['GIFT_BASKET'] / curr_positions['GIFT_BASKET'],
                                           available_bid_sizes['CHOCOLATE'] / curr_positions['CHOCOLATE'],
