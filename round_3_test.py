1e2fd02983c43b35beb250f2a63fb660cca5e1c4
aemilani <aealieftekhary@gmail.com>
2024-04-19 01:59:16 +0200

    2024-04-19 01:59

--- round_3_test.py
+++ round_3_test.py
@@ -9,171 +9,9 @@ class Trader:
     def run(self, state: TradingState):
         conversions = 0
         traderData = 'SAMPLE'
-        # Retrieve previous state -------------------------------------------------------------------------------------
-        if state.traderData:
-            try:
-                previous_state = jsonpickle.decode(state.traderData)
-                bids = previous_state.get('bids', {})
-                asks = previous_state.get('asks', {})
-            except Exception:
-                print('JSON Decode error encountered.')
-                bids = {}
-                asks = {}
-        else:
-            bids = {}
-            asks = {}
 
         # Trading -----------------------------------------------------------------------------------------------------
         result = {}
-
-        # AMETHYSTS ---------------------------------------------------------------------------------------------------
-        if 'AMETHYSTS' in state.order_depths:
-            product = 'AMETHYSTS'
-            print(product)
-
-            if product in state.position:
-                if state.position[product]:
-                    curr_position: int = state.position[product]
-                else:
-                    curr_position = 0
-            else:
-                curr_position = 0
-
-            print(f'Current position: {curr_position}')
-
-            fair_value = 10000
-            max_position = 20
-
-            max_buy_size = max_position - curr_position
-            max_sell_size = -max_position - curr_position
-
-            # Market making
-            thr_l = fair_value - 2
-            thr_h = fair_value + 2
-            buy_price = thr_l
-            sell_price = thr_h
-
-            orders: List[Order] = []
-            if max_buy_size > 0:
-                print("BUY", str(max_buy_size) + "x", buy_price)
-                orders.append(Order(product, buy_price, max_buy_size))
-            if max_sell_size < 0:
-                print("SELL", str(max_sell_size) + "x", sell_price)
-                orders.append(Order(product, sell_price, max_sell_size))
-
-            result[product] = orders
-
-        # STARFRUIT ---------------------------------------------------------------------------------------------------
-        if 'STARFRUIT' in state.order_depths:
-            product = 'STARFRUIT'
-            print(product)
-            if product not in bids:
-                bids[product] = []
-            if product not in asks:
-                asks[product] = []
-
-            order_depth: OrderDepth = state.order_depths[product]
-
-            if product in state.position:
-                if state.position[product]:
-                    curr_position: int = state.position[product]
-                else:
-                    curr_position = 0
-            else:
-                curr_position = 0
-
-            print(f'Current position: {curr_position}')
-
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-            else:
-                best_bid, best_bid_amount = None, None
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-            else:
-                best_ask, best_ask_amount = None, None
-
-            bids[product].append(best_bid)
-            asks[product].append(best_ask)
-
-            if len(bids[product]) > 100:
-                bids[product] = bids[product][1:]
-                asks[product] = asks[product][1:]
-
-            traderData = jsonpickle.encode({"bids": bids, "asks": asks})
-
-            max_position = 20
-            max_buy_size = min(max_position, max_position - curr_position)
-            max_sell_size = max(-max_position, -max_position - curr_position)
-
-            # Market making
-            bid_arr = np.array(bids[product])
-            ask_arr = np.array(asks[product])
-
-            orders: List[Order] = []
-            w_roll = 9
-            if len(bid_arr) >= w_roll:
-                mid_arr = (bid_arr + ask_arr) / 2
-                mid_ma = mid_arr[-w_roll:].mean()
-                fair_value = mid_ma
-                if max_buy_size > 0:
-                    buy_price = int(np.floor(fair_value)) - 1
-                    print("BUY", str(max_buy_size) + "x", buy_price)
-                    orders.append(Order(product, buy_price, max_buy_size))
-                if max_sell_size < 0:
-                    sell_price = int(np.ceil(fair_value)) + 1
-                    print("SELL", str(max_sell_size) + "x", sell_price)
-                    orders.append(Order(product, sell_price, max_sell_size))
-
-            result[product] = orders
-
-        # ORCHIDS -----------------------------------------------------------------------------------------------------
-        if 'ORCHIDS' in state.order_depths:
-            product = 'ORCHIDS'
-            print(product)
-
-            order_depth: OrderDepth = state.order_depths[product]
-
-            if product in state.position:
-                if state.position[product]:
-                    curr_position: int = state.position[product]
-                else:
-                    curr_position = 0
-            else:
-                curr_position = 0
-
-            print(f'Current position: {curr_position}')
-
-            conversions = -curr_position
-
-            max_position = 100
-            max_buy_size = max_position - curr_position
-            max_sell_size = -max_position - curr_position
-
-            bid_price = state.observations.conversionObservations['ORCHIDS'].bidPrice
-            ask_price = state.observations.conversionObservations['ORCHIDS'].askPrice
-            transport_fees = state.observations.conversionObservations['ORCHIDS'].transportFees
-            export_tariff = state.observations.conversionObservations['ORCHIDS'].exportTariff
-            import_tariff = state.observations.conversionObservations['ORCHIDS'].importTariff
-
-            # Arbitrage
-            effective_south_bid_price = bid_price - transport_fees - export_tariff
-            effective_south_ask_price = ask_price + transport_fees + import_tariff
-
-            north_bid_1, north_bid_1_amount = list(order_depth.buy_orders.items())[0]
-            north_ask_1, north_ask_1_amount = list(order_depth.sell_orders.items())[0]
-
-            orders: List[Order] = []
-            if north_bid_1 > effective_south_ask_price:  # short
-                sell_size = max(max_sell_size, -north_bid_1_amount)
-                print("SELL", str(sell_size) + "x", north_bid_1)
-                orders.append(Order(product, north_bid_1, sell_size))
-            elif north_ask_1 < effective_south_bid_price:  # long
-                buy_size = min(max_buy_size, -north_ask_1_amount)
-                print("BUY", str(buy_size) + "x", north_ask_1)
-                orders.append(Order(product, north_ask_1, buy_size))
-
-            result[product] = orders
 
         # GIFT_BASKET -------------------------------------------------------------------------------------------------
         if ('CHOCOLATE' in state.order_depths) and ('STRAWBERRIES' in state.order_depths) and\
@@ -258,7 +96,7 @@ class Trader:
                         sell_size -= curr_size
                 result[product].extend(orders)
 
-            mid_prices, best_bid_sizes, best_ask_sizes, max_buy_sizes, max_sell_sizes = {}, {}, {}, {}, {}
+            mid_prices, available_bid_sizes, available_ask_sizes, max_buy_sizes, max_sell_sizes = {}, {}, {}, {}, {}
             for product in products:
                 result[product] = []
 
@@ -271,26 +109,34 @@ class Trader:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
                 mid_prices[product] = (best_bid + best_ask) / 2
 
-                bid, best_bid_size = list(order_depth.buy_orders.items())[0]
-                best_bid_sizes[product] = best_bid_size
+                # available_bid_size = 0
+                # for i in range(len(order_depth.buy_orders)):
+                #     bid, bid_amount = list(order_depth.buy_orders.items())[i]
+                #     available_bid_size += bid_amount
+                bid, available_bid_size = list(order_depth.buy_orders.items())[0]
+                available_bid_sizes[product] = available_bid_size
 
-                ask, best_ask_size = list(order_depth.sell_orders.items())[0]
-                best_ask_sizes[product] = best_ask_size
+                # available_ask_size = 0
+                # for i in range(len(order_depth.sell_orders)):
+                #     ask, ask_amount = list(order_depth.sell_orders.items())[i]
+                #     available_ask_size += ask_amount
+                ask, available_ask_size = list(order_depth.sell_orders.items())[0]
+                available_ask_sizes[product] = available_ask_size
 
                 max_position = max_positions[product]
                 max_buy_size = max_position - curr_position
-                max_buy_size = min(max_buy_size, -best_ask_size)
+                max_buy_size = min(max_buy_size, -available_ask_size)
                 max_sell_size = -max_position - curr_position
-                max_sell_size = max(max_sell_size, -best_bid_size)
+                max_sell_size = max(max_sell_size, -available_bid_size)
                 max_buy_sizes[product] = max_buy_size
                 max_sell_sizes[product] = max_sell_size
 
-            best_bid_sizes['COMPOSITE'] = min(best_bid_sizes['CHOCOLATE'] // product_weight['CHOCOLATE'],
-                                              best_bid_sizes['STRAWBERRIES'] // product_weight['STRAWBERRIES'],
-                                              best_bid_sizes['ROSES'] // product_weight['ROSES'])
-            best_ask_sizes['COMPOSITE'] = max(best_ask_sizes['CHOCOLATE'] // product_weight['CHOCOLATE'],
-                                              best_ask_sizes['STRAWBERRIES'] // product_weight['STRAWBERRIES'],
-                                              best_ask_sizes['ROSES'] // product_weight['ROSES'])
+            available_bid_sizes['COMPOSITE'] = min(available_bid_sizes['CHOCOLATE'] // product_weight['CHOCOLATE'],
+                                                   available_bid_sizes['STRAWBERRIES'] // product_weight['STRAWBERRIES'],
+                                                   available_bid_sizes['ROSES'] // product_weight['ROSES'])
+            available_ask_sizes['COMPOSITE'] = max(available_ask_sizes['CHOCOLATE'] // product_weight['CHOCOLATE'],
+                                                   available_ask_sizes['STRAWBERRIES'] // product_weight['STRAWBERRIES'],
+                                                   available_ask_sizes['ROSES'] // product_weight['ROSES'])
             max_buy_sizes['COMPOSITE'] = min(max_buy_sizes['CHOCOLATE'] // product_weight['CHOCOLATE'],
                                              max_buy_sizes['STRAWBERRIES'] // product_weight['STRAWBERRIES'],
                                              max_buy_sizes['ROSES'] // product_weight['ROSES'])
@@ -300,8 +146,8 @@ class Trader:
 
             gift = mid_prices['GIFT_BASKET']
             comp = mid_prices['CHOCOLATE'] * product_weight['CHOCOLATE'] +\
-                   mid_prices['STRAWBERRIES'] * product_weight['STRAWBERRIES'] +\
-                   mid_prices['ROSES'] * product_weight['ROSES']
+                       mid_prices['STRAWBERRIES'] * product_weight['STRAWBERRIES'] +\
+                       mid_prices['ROSES'] * product_weight['ROSES']
             ratio = gift / comp
             ratio_mean = 1.2110
             ratio_std = 0.0013
@@ -318,6 +164,8 @@ class Trader:
             if ratio < thr_low:  # buy GIFT_BASKET, sell COMPOSITE
                 size_gift = min(max_buy_sizes['GIFT_BASKET'], -max_sell_sizes['COMPOSITE'] / ratio)
                 size_comp = -ratio * size_gift
+                # size_gift = int(size_gift)
+                # size_comp = int(size_comp)
                 size_gift = int(size_gift)
                 size_comp = int(size_comp)
                 if (size_gift != 0) and (size_comp != 0):
@@ -329,6 +177,8 @@ class Trader:
             elif ratio > thr_high:  # sell GIFT_BASKET, buy COMPOSITE
                 size_gift = max(max_sell_sizes['GIFT_BASKET'], -max_buy_sizes['COMPOSITE'] / ratio)
                 size_comp = -ratio * size_gift
+                # size_gift = int(size_gift)
+                # size_comp = int(size_comp)
                 size_gift = int(size_gift)
                 size_comp = int(size_comp)
                 if (size_gift != 0) and (size_comp != 0):
